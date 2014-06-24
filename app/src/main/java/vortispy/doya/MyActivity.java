20828b4d778ad09f05abf99a20e4d5e3b75dab42
vortispy <vortispy@users.noreply.github.com>
2014-06-24 16:16:03 +0900

    use jedis

--- app/src/main/java/vortispy/doya/MyActivity.java
+++ app/src/main/java/vortispy/doya/MyActivity.java
@@ -1,6 +1,7 @@
 package vortispy.doya;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -9,14 +10,18 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import redis.clients.jedis.Jedis;
+
 
 public class MyActivity extends Activity {
     private class DoyaPlus{
         int point = 0;
         int id;
+        String name;
 
-        DoyaPlus(int id){
+        DoyaPlus(int id, String name){
             this.id = id;
+            this.name = name;
         }
         public int getId(){
             return id;
@@ -30,11 +35,15 @@ public class MyActivity extends Activity {
         public void downPoint(){
             point--;
         }
+        public String getName(){
+            return this.name;
+        }
     }
 
     int nowId = 0;
-    final DoyaPlus doyas[] = {new DoyaPlus(R.drawable.dog1),new DoyaPlus(R.drawable.dog2),new DoyaPlus(R.drawable.dog3)};
+    final DoyaPlus doyas[] = {new DoyaPlus(R.drawable.dog1, "dog1"),new DoyaPlus(R.drawable.dog2, "dog2"),new DoyaPlus(R.drawable.dog3, "dog3")};
 
+    protected AsyncJedis asjd;
     protected int getNowId(){
         return nowId;
     }
@@ -49,17 +58,22 @@ public class MyActivity extends Activity {
         final ImageView image = (ImageView) findViewById(R.id.imageView);
         image.setImageResource(R.drawable.dog1);
         final TextView point = (TextView) findViewById(R.id.pointView);
+
+        for (DoyaPlus doya : doyas) {
+            asjd = new AsyncJedis(point);
+            asjd.execute("set", doya.getName(), "0");
+        }
+
         point.setText(String.valueOf(doyas[nowId].getPoint()));
 
         Button nextButton = (Button) findViewById(R.id.button);
         nextButton.setOnClickListener(new View.OnClickListener() {
-
-
             @Override
             public void onClick(View view) {
                 nextId();
                 image.setImageResource(doyas[nowId].getId());
-                point.setText(String.valueOf(doyas[nowId].getPoint()));
+                asjd = new AsyncJedis(point);
+                asjd.execute("get", doyas[nowId].getName());
             }
         });
 
@@ -67,8 +81,9 @@ public class MyActivity extends Activity {
         plusButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                doyas[getNowId()].upPoint();
-                point.setText(String.valueOf(doyas[nowId].getPoint()));
+                //doyas[getNowId()].upPoint();
+                asjd = new AsyncJedis(point);
+                asjd.execute("incr", doyas[nowId].getName());
             }
         });
 
@@ -76,8 +91,9 @@ public class MyActivity extends Activity {
         minusButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                doyas[getNowId()].downPoint();
-                point.setText(String.valueOf(doyas[nowId].getPoint()));
+                //doyas[getNowId()].downPoint();
+                asjd = new AsyncJedis(point);
+                asjd.execute("decr", doyas[nowId].getName());
             }
         });
 
@@ -103,4 +119,41 @@ public class MyActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public class AsyncJedis extends AsyncTask<String, String, String>{
+        Jedis jd;
+        TextView textView;
+        public AsyncJedis(TextView textView){
+            super();
+
+            this.textView = textView;
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            this.jd = new Jedis("10.0.2.2");
+            String ret = "";
+            String method = strings[0];
+            if (method.equals("get")) {
+                ret = this.jd.get(strings[1]);
+            }else if (method.equals("set")){
+                ret = this.jd.set(strings[1], strings[2]);
+            }else if (method.equals("incr")){
+                this.jd.incr(strings[1]);
+                ret = this.jd.get(strings[1]);
+            }else if (method.equals("decr")){
+                this.jd.decr(strings[1]);
+                ret = this.jd.get(strings[1]);
+            }
+            return ret;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            this.textView.setText(s);
+            this.jd.disconnect();
+        }
+    }
+
 }
